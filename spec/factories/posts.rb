12939a97593d02b7bc7de8d9b3a21bdc82dfaea7
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-06-21 16:50:35 -0400

    Added hours requested to post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,18 +2,14 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "some rationale"
+    overtime_request 3.5
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "some more content"
+    overtime_request 0.5
     user
-  end
-
-  factory :post_from_other_user, class: "Post" do
-    date Date.yesterday
-    rationale "some more content"
-    user { FactoryGirl.create(:non_authorized_user) }
   end
 end
