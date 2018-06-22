fca8baa5d4ce3f7bb8cfa56efc7188b96496545f
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-06-22 16:22:50 -0400

    Added phone attribute to User and made it required

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-@user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "test@test.com",
+                    password: "password",
+                    password_confirmation: "password",
+                    first_name: "Jon",
+                    last_name: "Snow",
+                    phone: "3526651142")
 
-puts "1 user created"
+        puts "1 user created"
 
-AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "Name")
+        AdminUser.create(email: "admin@test.com",
+                         password: "password",
+                         password_confirmation: "password",
+                         first_name: "Admin",
+                         last_name: "Name",
+                         phone: "3526651142")
 
-puts "1 Admin User created"
+        puts "1 Admin User created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
-end
+        100.times do |post|
+          Post.create!(date: Date.today,
+                       rationale: "#{post} rationale content",
+                       user_id: @user.id,
+                       overtime_request: 2.5)
+        end
 
-puts "100 post have been created"
+          puts "100 post have been created"
