ef6450bfa56e6be702fd5ffc2e80d5739d2d3d1b
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-07-13 16:43:00 -0400

    Trying sendgrid smtp settings in production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -12,13 +12,12 @@
 # end
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.gmail.com',
+  :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['GMAIL_USERNAME'],
-  :password => ENV['GMAIL_PASSWORD'],
-  # :domain => 'heroku.com',
-  :enable_starttls_auto => true,
-  # :openssl_verify_mode => `none`
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
