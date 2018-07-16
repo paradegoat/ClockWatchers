e579961d4521fab23f1e71512d80e67952f8073c
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-07-16 15:17:36 -0400

    Refactored button functionality for employee dashboard

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,10 +3,9 @@ class AuditLog < ApplicationRecord
 
   belongs_to :user
   validates_presence_of :user_id, :status, :start_date
-
   after_initialize :set_defaults
-
   before_update :set_end_date, if: :confirmed?
+  scope :by_start_date, -> { order('start_date DESC') }
 
   private
 
